9ef92358e136fea24109b63f64c1667339bdf34b
Rafael Araujo <rafa123839@gmail.com>
2022-10-04 16:39:54 -0300

    Interface chamado criado

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,13 @@
+export interface Chamado {
+    id?: any;
+    dataabertur?: string;
+    dataFechamento?: string;
+    prioridade: string;
+    status: string;
+    titulo: string;
+    descricao: string;
+    tecnico: any;
+    cliente: any;
+    nomeCliente: string;
+    nomeTecnico: string;
+}
